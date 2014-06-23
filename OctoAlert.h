24ea33e8f7bbffa51703fbf101a2f25fd09321dd
Nicolas-iMac <nicolaslambert@free.fr>
2014-06-23 11:10:18 +0200

    New project OctoAlert: First music tests

---
+++ OctoAlert.h
@@ -0,0 +1,53 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _OctoAlert_H_
+#define _OctoAlert_H_
+#include "Arduino.h"
+//add your includes for the project OctoAlert here
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+
+
+// define the pins used
+//#define CLK 13       // SPI Clock, shared with SD card
+//#define MISO 12      // Input data, from VS1053/SD card
+//#define MOSI 11      // Output data, to VS1053/SD card
+// Connect CLK, MISO and MOSI to hardware SPI pins.
+// See http://arduino.cc/en/Reference/SPI "Connections"
+
+// These are the pins used for the breakout example
+#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
+#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
+#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
+// These are the pins used for the music maker shield
+#define SHIELD_CS     7      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+
+// These are common pins between breakout and shield
+#define CARDCS 4     // Card chip select pin
+// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project OctoAlert here
+
+
+
+
+//Do not add code below this line
+#endif /* _OctoAlert_H_ */
---
+++ spec.d
@@ -0,0 +1,2 @@
+spec.o:  \
+ /Users/nicolaslambert/Documents/EclipseWorkspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
